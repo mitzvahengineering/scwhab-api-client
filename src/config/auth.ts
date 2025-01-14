de1e1cb129f6653db57e77280a8a477bad82022d
munair <munair@gmail.com>
2025-01-14 19:26:40 +0900

    switching to https://schwab.mitzvah.capital/callback

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -5,7 +5,7 @@ const clientSecret = import.meta.env.VITE_SCHWAB_CLIENT_SECRET
 export const AUTH_CONFIG = {
   clientId: clientId as string,
   clientSecret: clientSecret as string,
-  redirectUri: 'https://127.0.0.1:5173/callback',
+  redirectUri: 'https://schwab.mitzvah.capital/callback',
   authorizationEndpoint: 'https://api.schwabapi.com/oauth/authorize',
   tokenEndpoint: 'https://api.schwabapi.com/oauth/token',
   apiEndpoint: 'https://api.schwabapi.com/marketdata/v1'
