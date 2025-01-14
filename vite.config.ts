a90b7745245bf90314a1d53cb1ec05bb50d423c2
munair <munair@gmail.com>
2025-01-14 18:46:49 +0900

    update the config to use the self-signed certificate for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   server: {
-    https: true,
-    port: 5173
+    port: 5173,
+    https: true
   },
   resolve: {
     alias: {
