4b8a6cbf3dda1d6802a5683ef992752490be6614
munair <munair@gmail.com>
2025-01-14 19:30:41 +0900

    simplify package json for AWS Amplify

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,17 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  server: {
-    port: 5173,
-    https: true,
-    host: '127.0.0.1', // Force IPv4
-    strictPort: true   // Don't try other ports if 5173 is taken
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
     }
-  }
+  },
+  base: '/' // This ensures all assets are loaded from the root path
 })
